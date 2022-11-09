8ae05228570f16d81882e0af562f96fbeb5dbea3
Pedro Germano <pogermano@gmail.com>
2022-11-09 10:04:52 -0300

    Cadastro de usuário laravel

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,15 +5,42 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function store(Request $request) {
         $validator = Validator::make($request-> all(), [
             'name' => 'required|string|max:255',
-            'email' =>
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6'
+
 
         ]);
+        if($validator->fails() ){
+            return response()->json(['errors' => $validator->errors()],200);
+        }
 
+        $date = Carbon::now();
+        $delete_account = Carbon::now();//clone($date)
+
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->next_expiration = $date-> addDays(7);
+        $user->delete_account = $delete_account-> addDays(15);
+        $user->save();
+
+        if ($user->id) {
+            return response()->json([
+                'access_token'=> $user->createToken('auth-api')->accessToken
+            ],200);
+        }
+        return response()->json([
+            'error'=> 'Erro ao cadastrar usuário'
+        ],200);
     }
 }
